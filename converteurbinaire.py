16c0bf6e256e55104e619bf503b813fc488bd4cf
Taner1998 <45757068+Taner1998@users.noreply.github.com>
2018-12-20 10:12:49 +0100

    Update converteurbinaire.py

--- converteurbinaire.py
+++ converteurbinaire.py
@@ -12,5 +12,10 @@ def main():
 		n += 1
 
 		print("2*",val_numbers[n],"+",r,"q_n=",div,"r_n=",r)
+	while n>0:
+		n -= 1
+		binaire = rest[n]
+		s.append(binaire)
+	print(val_numbers[0],"_10","=" ,s)
 if __name__ == "__main__":
     main()
