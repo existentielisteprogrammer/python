b15ed2a0f686fa89171e6a176429df1aee2875aa
Taner1998 <taberandwords@hotmail.com>
2021-07-27 17:18:31 +0400

    rebuilded after 3 years

--- armstrong.py
+++ armstrong.py
@@ -1,14 +1,34 @@
-a = 1
-b = 0
-c = 0
-n = int(input("arm"))
-while a<=9:
-    a+=1
-    while b<=9:
-        b+=1
-        while c<9:
-            c+=1
-            arm = a * 100 + b * 10 + c
-            armcube = a * a * a + b * b * b + c * c * c
-if arm==armcube:
-    print(arm)
+
+def armstrong_validator(length):
+    """This function will create a number from scratch in selected length and given integers 
+    by user than check if produced number is an armstrong number or not."""
+    count=0
+    number=[] 
+    while count<length:
+        value=int(input("Dear user please write an integer between i>=0 and i<10 : "))
+        number.append(value)
+        count+=1
+        
+        continue
+        if count==len:
+            break
+        count+=1
+    test_number=0
+    for i in range(count):
+            count-=1
+            test_number+=number[i]*10** count
+            #print(number[i],count)
+
+    total=0
+    count=len(number)
+    for i in range(count):
+        total+=number[i]**count
+    if(total==test_number):
+        print(test_number, ' is an armstrong number!')
+    else:
+        print('sorry',test_number, 'is not an armstrong number!')
+    
+armstrong_validator(int(input('give a length please : ')))
+
+        
+        
